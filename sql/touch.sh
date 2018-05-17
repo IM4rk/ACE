3579e7f016cbf51f6e128a2b7ce86f6354dcdb8d
oracle <oracle@edu-dev-db20.decc.local>
2018-05-17 10:41:29 +0100

    original code

---
+++ sql/touch.sh
@@ -0,0 +1,6 @@
+
+#!/bin/sh                                                                                                                                                                                               
+
+touch /oracle/fra/FNCL69/fncl691.log                                                                                                                                                                    
+touch /oracle/fra/FNCL69/fncl692.log                                                                                                                                                                    
+touch /oracle/fra/FNCL69/fncl693.log                                                                                                                                                                    
